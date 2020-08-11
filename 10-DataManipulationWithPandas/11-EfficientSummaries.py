2ce7b60dbaa98200f13fb019d1dbc064e5941b79
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-11 21:28:29 +0530

    Add files via upload

---
+++ 10-DataManipulationWithPandas/11-EfficientSummaries.py
@@ -0,0 +1,28 @@
+#part1
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+
+# Print IQR of the temperature_c column
+print(sales["temperature_c"].agg(iqr))
+
+
+#part2
+# Import NumPy and create custom IQR function
+import numpy as np
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg(iqr))
+
+
+#part3
+# Import NumPy and create custom IQR function
+import numpy as np
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, np.median]))
