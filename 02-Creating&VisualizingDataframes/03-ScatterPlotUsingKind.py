3ad60b0f6cf807973a56b7550ae9f682c4fa29b9
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-16 13:16:46 +0530

    Add files via upload

---
+++ 02-Creating&VisualizingDataframes/03-ScatterPlotUsingKind.py
@@ -0,0 +1,5 @@
+# Scatter plot of nb_sold vs avg_price with title
+avocados.plot(x="nb_sold" , y="avg_price" , title="Number of avocados sold vs. average price",kind="scatter")
+
+# Show the plot
+plt.show()
