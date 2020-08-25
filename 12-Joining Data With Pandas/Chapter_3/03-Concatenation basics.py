4593df2408ec80dea00348aaf76db8759fb9689f
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-08-25 09:43:57 +0530

    Add files via upload

---
+++ 12-Joining Data With Pandas/Chapter_3/03-Concatenation basics.py
@@ -0,0 +1,21 @@
+#part1
+# Concatenate the tracks
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               sort=True)
+print(tracks_from_albums)
+
+
+#part2
+# Concatenate the tracks so the index goes from 0 to n-1
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               ignore_index=True,
+                               sort=True)
+print(tracks_from_albums)
+
+
+#part3
+# Concatenate the tracks, show only columns names that are in all tables
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               join='inner',
+                               sort=True)
+print(tracks_from_albums)
