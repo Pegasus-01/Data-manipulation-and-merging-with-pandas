70f27f6b227e1e3faf177ac157331d1f55e4ee25
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-09-01 21:28:21 +0530

    Add files via upload

---
+++ 13-Introduction to Data Visualization with Matplotlib/Chapter_4/02-Saving a file several times.py
@@ -0,0 +1,12 @@
+#part1 datasets from another directory,datacamp console works
+Saving a file several times
+
+
+#part2
+# Save as a PNG file
+fig.savefig('my_figure.png')
+
+
+#part3
+# Save as a PNG file with 300 dpi
+fig.savefig('my_figure_300dpi.png',dpi=300)
