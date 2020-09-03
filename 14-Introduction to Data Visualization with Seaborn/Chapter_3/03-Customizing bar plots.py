5bef516213fa36e4150ede23c5b6b6b55a9b0d68
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-09-03 07:26:45 +0530

    Add files via upload

---
+++ 14-Introduction to Data Visualization with Seaborn/Chapter_3/03-Customizing bar plots.py
@@ -0,0 +1,36 @@
+#part1
+# Create bar plot of average final grade in each study category
+sns.catplot(x="study_time",y="G3",data=student_data,kind="bar")
+
+
+
+# Show plot
+plt.show()
+
+
+#part 2
+# Rearrange the categories
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=["<2 hours",
+                   "2 to 5 hours",
+                   "5 to 10 hours",
+                   ">10 hours"])
+
+# Show plot
+plt.show()
+
+
+#part3
+# Turn off the confidence intervals
+sns.catplot(x="study_time", y="G3",
+            data=student_data,
+            kind="bar",
+            order=["<2 hours",
+                   "2 to 5 hours",
+                   "5 to 10 hours",
+                   ">10 hours"],ci=None)
+
+# Show plot
+plt.show()
